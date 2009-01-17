94309927f34ffa3f26904af9c43e95cdd8ce399f
unknown <Waldemar@.(none)>
2009-01-16 21:49:21 -0600

    primer intento de hacer la tarea

---
+++ IStack.cs
@@ -0,0 +1,11 @@
+// interfaz IStack.cs
+
+public interface IStack{
+  // los métodos que se van a declarar
+  // push algo al stack.
+  // pop algo al stack.
+  // decir cuantos te quedan
+  void popStack();
+  void pushStack();
+  void howMany();
+}
---
+++ StackImpl1.cs
@@ -0,0 +1,23 @@
+// implementacion de la interfaz IStack.cs
+
+public class StackImpl1 : IStack{
+  
+  private int cosasEnStack;
+  private int capacidad;
+  private int[] elStack;
+  // declarar un arreglo...
+  
+  public void popStack(){
+    
+  }
+  public void pushStack(){
+    
+  }
+  public void howMany(){
+    
+  }
+  static void Main(){
+    System.Console.WriteLine("sirve esto?");
+  }
+}
+
